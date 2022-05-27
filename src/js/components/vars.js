563765e9d35998fa47e989ffc6aa8451c9dcd624
Sergei Birukov <suxscribe@gmail.com>
2022-05-27 15:24:58 +0300

    Update vars.js
    added facebook / twitter share urls

--- src/js/components/vars.js
+++ src/js/components/vars.js
@@ -6,4 +6,6 @@ export const vars = {
   postTeaserClass: 'post',
   modalVideoId: 'modal-video',
   likeArtworkInput: '#like-artwork',
+  facebookShareUrl: 'https://www.facebook.com/sharer/sharer.php?u=',
+  twitterShareUrl: 'http://twitter.com/share?url=',
 };
