b7c11f0c30fe094af8bcea1dbd965dc3bfdd0ab3
Sergei Birukov <suxscribe@gmail.com>
2022-05-27 15:22:35 +0300

    Update modal-like.js
    Added sharing links generation on like modal open

--- src/js/components/modal-like.js
+++ src/js/components/modal-like.js
@@ -6,14 +6,29 @@ export const modalLike = () => {
 
 const addArtworkLinkToLikeForm = (e) => {
   if (e.target.matches('.gallery__item-like')) {
-    // insert artwork url to like form hidden input
-
     const likeButtonParent = e.target.parentNode;
     const artworkLink = likeButtonParent.querySelector('.gallery__item-link');
     const artworkUrl = artworkLink.getAttribute('href');
     console.log(artworkUrl);
 
+    // insert artwork url to like form hidden input
     const likeArtworkInput = document.querySelector(vars.likeArtworkInput);
     likeArtworkInput.value = artworkUrl;
+
+    // todo add artwork url to sharing links
+    const likeArtworkFacebook = document.querySelector(
+      '.modal__social--facebook'
+    );
+    const likeArtworkTwitter = document.querySelector(
+      '.modal__social--twitter'
+    );
+
+    if (likeArtworkFacebook)
+      likeArtworkFacebook.href =
+        vars.facebookShareUrl + encodeURIComponent(artworkUrl);
+
+    if (likeArtworkTwitter)
+      likeArtworkTwitter.href =
+        vars.twitterShareUrl + encodeURIComponent(artworkUrl);
   }
 };
