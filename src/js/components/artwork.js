927befbb3a8056c7b818f01800c8b470da33d29d
Sergei Birukov <suxscribe@gmail.com>
2022-05-27 15:21:21 +0300

    Update artwork.js
    Added generating sharing links
    Added moving size, video, like content into outside modals

--- src/js/components/artwork.js
+++ src/js/components/artwork.js
@@ -3,27 +3,61 @@ import ResizeSensorMin from 'resize-sensor';
 import StickySidebar from 'sticky-sidebar-v2';
 import { vars } from './vars';
 import ArtworkGallery from './ArtworkGallery';
+import { artworkModalOverflow, modals } from './modals';
 
 export default class Artwork {
   constructor(_options) {
     this.gallerySelector = '.artwork__image';
+    this.artworkUrl = _options.url;
+
+    // containers for artwork size, video, gallery modals
+    this.modalSizeContainer = document.querySelector('.modal__content--size');
+    this.modalSizeContainer.innerHTML = '';
+    this.modalVideoContainer = document.querySelector('.modal__content--video');
+    this.modalVideoContainer.innerHTML = '';
+    this.modalGalleryContainer = document.querySelector('.pswp-container');
+    this.modalGalleryContainer.innerHTML = '';
+    console.log(this.modalGalleryContainer);
+
     this.init();
   }
 
   init() {
     this.initArtworkGallery();
-    this.initArtworkSizeCompare();
+    // this.initArtworkSizeCompare();
     this.initArtworkStickySidebar();
     this.initArtworkEventListeners();
+
+    console.log('url', this.artworkUrl);
   }
 
   initArtworkGallery() {
+    const pswpContainer = document.querySelector('.pswp');
+
+    this.modalGalleryContainer.append(pswpContainer);
     this.artworkGallery = new ArtworkGallery();
   }
 
   initArtworkSizeCompare() {
-    const reference = document.querySelector('.artwork__size-reference img');
-    const artwork = document.querySelector('.artwork__size-work img');
+    // get modal-size element
+    // const modalSize = document.querySelector('.modal__content--size');
+
+    if (this.modalSizeContainer) {
+      // modalSize.innerHTML = '';
+
+      const artworkSizeContent = document.querySelector('.artwork__size');
+      this.modalSizeContainer.append(artworkSizeContent);
+      // modalSize.innerHTML = artworkSizeContent.innerHTML;
+    }
+
+    // get artwork size content
+    // put artwork size content in modal-size
+    const reference = this.modalSizeContainer.querySelector(
+      '.artwork__size-reference img'
+    );
+    const artwork = this.modalSizeContainer.querySelector(
+      '.artwork__size-work img'
+    );
 
     if (reference && artwork) {
       const referenceRealHeight = reference.dataset.height;
@@ -47,9 +81,13 @@ export default class Artwork {
       MicroModal.show('modal-size', {
         awaitOpenAnimation: true,
         awaitCloseAnimation: true,
-        // onShow: () => {
-        //   setTimeout(artworkSizeCompare, 2000);
-        // },
+        onShow: () => {
+          // setTimeout(artworkSizeCompare, 2000);
+          // artworkModalOverflow(false);
+        },
+        onClose: () => {
+          // artworkModalOverflow(true);
+        },
       });
       this.initArtworkSizeCompare();
     }
@@ -61,11 +99,38 @@ export default class Artwork {
         awaitOpenAnimation: true,
         awaitCloseAnimation: true,
       });
+
+      // set artwork url to input#like-artwork
+      const likeArtworkInput = document.querySelector(vars.likeArtworkInput);
+      likeArtworkInput.value = this.artworkUrl;
+
+      const likeArtworkFacebook = document.querySelector(
+        '.modal__social--facebook'
+      );
+      const likeArtworkTwitter = document.querySelector(
+        '.modal__social--twitter'
+      );
+
+      if (likeArtworkFacebook)
+        likeArtworkFacebook.href =
+          vars.facebookShareUrl + encodeURIComponent(this.artworkUrl);
+
+      if (likeArtworkTwitter)
+        likeArtworkTwitter.href =
+          vars.twitterShareUrl + encodeURIComponent(this.artworkUrl);
     }
 
     if (e.target.matches('.artwork__icon--video')) {
       e.preventDefault();
       e.stopPropagation();
+
+      if (this.modalVideoContainer) {
+        // modalSize.innerHTML = '';
+
+        const artworkVideoContent = document.querySelector('.artwork__video');
+        this.modalVideoContainer.append(artworkVideoContent);
+        // modalSize.innerHTML = artworkSizeContent.innerHTML;
+      }
 
       const modalVideo = document.querySelector(`#${vars.modalVideoId} video`);
       if (modalVideo) {
