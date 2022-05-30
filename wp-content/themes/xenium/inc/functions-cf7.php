7aa9b1720f85f801a44aeb8db54338e56e341df9
Sergei Birukov <suxscribe@gmail.com>
2022-05-30 11:19:53 +0300

    Update functions-cf7.php
    Added ssl tweaks to cf7. To fix error on sending emails via SMTP on production hosting

--- wp-content/themes/xenium/inc/functions-cf7.php
+++ wp-content/themes/xenium/inc/functions-cf7.php
@@ -24,3 +24,15 @@ add_filter('wpcf7_form_elements', function ($content) {
 
   return $content;
 });
+
+add_filter('wp_mail_smtp_custom_options', function ($phpmailer) {
+  $phpmailer->SMTPOptions = array(
+    'ssl' => array(
+      'verify_peer'       => false,
+      'verify_peer_name'  => false,
+      'allow_self_signed' => true
+    )
+  );
+
+  return $phpmailer;
+});
